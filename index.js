4c573d919c69374b53c3d907a56cd9dc1ccb0ba5
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-05 14:18:51 -0400

    my solution

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+let countDown = (n) => {
+    for(let i = n; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+let writeCards = (arr) => {
+    let newCards = [];
+    for(let i = 0; i < arr.length; i++){
+        newCards.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    }
+    return newCards
+}
